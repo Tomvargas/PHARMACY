deadba3e618837d6bbc38da4bad8f08c882d33ad
tomvargas <tomvargasd@gmail.com>
2021-02-20 13:18:12 -0500

    add database items to listview

--- app/src/main/java/com/example/pedidos_farmaceuticos/basededatos.java
+++ app/src/main/java/com/example/pedidos_farmaceuticos/basededatos.java
@@ -16,7 +16,11 @@ import java.util.List;
 
 public class basededatos extends SQLiteOpenHelper {
     private static final String query = "CREATE TABLE IF NOT EXISTS pedido(medicina TEXT, tipo TEXT, cantidad TEXT, dist TEXT, suc TEXT);";
-
+    ArrayList<String> medicinas = new ArrayList<>();
+    ArrayList<String> tipos = new ArrayList<>();
+    ArrayList<String> cants = new ArrayList<>();
+    ArrayList<String> dists = new ArrayList<>();
+    ArrayList<String> sucs = new ArrayList<>();
 
     public basededatos(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -49,25 +53,37 @@ public class basededatos extends SQLiteOpenHelper {
         else return true;
     }
 
-    public List<Data> cursor(){
+    public ArrayList<ArrayList<String>> cursor(){
         SQLiteDatabase MyDB = this.getWritableDatabase();
-        List<Data> mySuperList = new ArrayList<>();
+        ArrayList<ArrayList<String>> data = new ArrayList<>();
+
+        medicinas.clear();
+        tipos.clear();
+        cants.clear();
+        dists.clear();
+        sucs.clear();
+
         Cursor cur = MyDB.rawQuery("Select * from pedido", null);
         if (cur != null){
             cur.moveToFirst();
             while (cur.isAfterLast() == false) {
-                Data myTable = new Data();
-                myTable.medicina = (cur.getString(cur.getColumnIndex("medicina")));
-                myTable.tipo = (cur.getString(cur.getColumnIndex("tipo")));
-                myTable.cant = (cur.getString(cur.getColumnIndex("cantidad")));
-                myTable.dist = (cur.getString(cur.getColumnIndex("dist")));
-                myTable.suc = (cur.getString(cur.getColumnIndex("suc")));
 
-                mySuperList.add(myTable);
+                medicinas.add(cur.getString(cur.getColumnIndex("medicina")));
+                tipos.add(cur.getString(cur.getColumnIndex("tipo")));
+                cants.add(cur.getString(cur.getColumnIndex("cantidad")));
+                dists.add(cur.getString(cur.getColumnIndex("dist")));
+                sucs.add(cur.getString(cur.getColumnIndex("suc")));
+
                 cur.moveToNext();
             }
         }
-        return mySuperList;
+        data.add(medicinas);
+        data.add(tipos);
+        data.add(cants);
+        data.add(dists);
+        data.add(sucs);
+
+        return data;
     }
 
     class Data{
--- app/src/main/java/com/example/pedidos_farmaceuticos/consulta.java
+++ app/src/main/java/com/example/pedidos_farmaceuticos/consulta.java
@@ -3,6 +3,7 @@ package com.example.pedidos_farmaceuticos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,9 +18,30 @@ public class consulta extends AppCompatActivity {
         basededatos db = new basededatos(this, "pedidos", null, 1);
 
         pedidos = findViewById(R.id.pedidos);
-        List<basededatos.Data> pe = db.cursor();
-        for(int i = 0; i< pe.size(); i++){
-            System.out.println(pe.get(i).toString());
+        ArrayList<ArrayList<String>> pe = db.cursor();
+
+        ArrayList<String> medicinas = new ArrayList<>();
+        ArrayList<String> tipos = new ArrayList<>();
+        ArrayList<String> cants = new ArrayList<>();
+        ArrayList<String> dists = new ArrayList<>();
+        ArrayList<String> sucs = new ArrayList<>();
+        ArrayList<String> datas = new ArrayList<>();
+
+        if (pe.size() > 0){
+            datas.clear();
+
+            medicinas = pe.get(0);
+            tipos = pe.get(1);
+            cants = pe.get(2);
+            dists = pe.get(3);
+            sucs = pe.get(4);
+
+            for(int i=0; i<medicinas.size(); i++){
+                datas.add(cants.get(i)+" unidades de "+tipos.get(i)+" "+medicinas.get(i)+" de la distribuidora "+dists.get(i)+" para, "+sucs.get(i));
+            }
+
+            final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas);
+            pedidos.setAdapter(arrayAdapter);
         }
     }
 }
